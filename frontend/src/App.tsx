1a439b4543b520ee546c7e6d72f7b7bc5fd9158c
Mohit Sharma <sharmamohit8624@gmail.com>
2026-04-15 13:03:00 +0530

    editing the axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,10 +26,7 @@ function App() {
     setCurrentShortLink(null)
 
     try {
-      const response = await axios.post("https://lnk-io-backend.onrender.com", {
-        originalUrl: originalUrl
-      })
-      
+      const response = await axios.post("https://lnk-io-backend.onrender.com", { originalUrl });
       const newLink = response.data;
       setCurrentShortLink(newLink);
       // Add the new link to the top of the history list
